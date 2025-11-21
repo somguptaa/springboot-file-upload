2f758e69ebf0eba2be283404c0bd86d19ad6695f
Som Gupta <somgupta0011@gmail.com>
2025-11-21 15:21:51 +0530

    Update FilesController.java

--- main/java/com/som/controller/FilesController.java
+++ main/java/com/som/controller/FilesController.java
@@ -17,12 +17,26 @@ import com.som.service.FilesService;
 import com.som.utility.Constants;
 import com.som.utility.ResponseMessageDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "FilesController Controller",description = "FileUpload Single, Multiple")
+
 @RestController
 public class FilesController {
 
     @Autowired
     private FilesService filesService;
 
+    @Operation(summary = "Single File-Upload",description = "Single File-Upload In The Database")
+    @ApiResponses({
+     @ApiResponse(responseCode = "201",description = "Single File-Upload Successfully"),
+     @ApiResponse(responseCode = "400",description = "Single File-Upload Failed"),
+     @ApiResponse(responseCode = "500",description = "Internal Server Error")})
+    
+    
     @PostMapping("/uploadfile")
     public ResponseEntity<ResponseMessageDto> uploadSingleFile(@RequestParam MultipartFile file) throws IOException {
 
@@ -30,9 +44,15 @@ public class FilesController {
         return ResponseEntity.ok(response);
     }
 
+    
+    @Operation(summary = "Multiple File-Upload",description = "Multiple File-Upload In The Database")
+    @ApiResponses({
+     @ApiResponse(responseCode = "201",description = "Multiple File-Upload Successfully"),
+     @ApiResponse(responseCode = "400",description = "Multiple File-Upload Failed"),
+     @ApiResponse(responseCode = "500",description = "Internal Server Error")})
+    
     @PostMapping("/uploadmultifile")
-    public ResponseEntity<ResponseMessageDto> uploadMultipleFile(@RequestParam MultipartFile[] filess)
-            throws IOException {
+    public ResponseEntity<ResponseMessageDto> uploadMultipleFile(@RequestParam MultipartFile[] filess)throws IOException {
 
         List<ResponseMessageDto> uploadedFiles = filesService.uploadMultipleFile(filess);
 
