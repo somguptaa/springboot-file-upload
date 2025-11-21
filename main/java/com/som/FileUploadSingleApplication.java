37af0cdb2cedb2fa2b32ecb3be416c45c9629eec
Som Gupta <somgupta0011@gmail.com>
2025-11-21 15:22:33 +0530

    Update FileUploadSingleApplication.java

--- main/java/com/som/FileUploadSingleApplication.java
+++ main/java/com/som/FileUploadSingleApplication.java
@@ -3,6 +3,18 @@ package com.som;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+        title = "File-Upload APIs",
+        version = "1.0",
+        description = "Single & Multiple File-Upload",
+        contact = @Contact(name ="Som Gupta",email = "somgupta0011@gmail.com")))
+
+
 @SpringBootApplication
 public class FileUploadSingleApplication {
 
